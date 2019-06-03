563e49dfb011a1b1c3fa35a17f115363a21ded05
sayan <anshu.p@encoders.co.in>
2019-06-03 13:25:33 +0530

    Add helper activity for FAQs of working with location in background

---
+++ locationfetchhelper/src/main/java/com/checkmyuniverse/locationfetchhelper/newsdk/HelpActivity.java
@@ -0,0 +1,16 @@
+package com.checkmyuniverse.locationfetchhelper.newsdk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.checkmyuniverse.locationfetchhelper.R;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
